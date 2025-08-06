96db163d8f36b9ebc77406c02b5860dbb8452baa
Yamil Font <yamil@imfont.com>
2025-08-05 19:42:35 -0600

    Add project files.

---
+++ KeyVaultAccessLib/KeyVaultClient.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Threading.Tasks;
+
+namespace KeyVaultAccessLib
+{
+    public class KeyVaultClient
+    {
+        #region Fields
+        private readonly SecretClient _client;
+        #endregion
+
+        #region Constructor
+        public KeyVaultClient(string vaultUrl)
+        {
+            Uri vaultUri = new Uri(vaultUrl);
+            _client = new SecretClient(vaultUri, new DefaultAzureCredential());
+        }
+        #endregion
+
+        #region Methods
+        public async Task<string> GetSecretAsync(string secretName)
+        {
+            Response<KeyVaultSecret> secret = await _client.GetSecretAsync(secretName);
+            return secret.Value.Value;
+        }
+        #endregion
+    }
+}
