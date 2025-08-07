a63c3ff1718675d46df61ddb0cf26694b8b32f56
Yamil Font <yamil@imfont.com>
2025-08-07 16:14:52 -0600

    added placeholder test to make sure workflow successfully tests it

--- FarmToTableTests/SubscriberUnitTests.cs
+++ FarmToTableTests/SubscriberUnitTests.cs
@@ -3,5 +3,13 @@ namespace FarmToTableTests
     [TestFixture]
     public class Tests
     {
+        [Test]
+        public void VerifyNothing_ReturnsTrue()
+        {
+            // just a placeholder test
+            bool a = true;
+            bool b = true;
+            Assert.That(a, Is.EqualTo(b));
+        }
     }
 }
